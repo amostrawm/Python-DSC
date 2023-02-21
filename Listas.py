9d00577c4f5d2157a1d780801e2991edd5c7f8c4
Amós Almeida <amsbeawp@gmail.com>
2023-02-21 18:13:41 -0300

    Adciona listas

---
+++ Listas.py
@@ -0,0 +1,13 @@
+#Listas
+
+lst = [1, 2, 3, 4, 5]
+lst2 = [1, 2, 3, "4", True]
+lst3 = [12, [1, 2, 3, 4, 5], "a"]
+
+#Pode-se crar uma lista automaticamente com o range desejado
+lst4 = list(range(0, 10))
+
+print(lst[0])
+print(lst2[4])
+print(lst3[2])
+print(lst4[1])
