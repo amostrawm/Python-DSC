619a1bbd4dce042847e6b2c9fb3709efebfbc712
Amós Almeida <amsbeawp@gmail.com>
2023-02-21 12:09:09 -0300

    Adciona For

--- Estruturas-de-repeticao.py
+++ Estruturas-de-repeticao.py
@@ -5,3 +5,12 @@ count = 1
 while count <= 5:
     print(count)
     count += 1
+
+#Estrutura de repetição For
+
+for i in range(0, 11):
+    print(i)
+
+#Adiciona incremento de 2
+for i in range(0, 11, 2):
+    print(i)
