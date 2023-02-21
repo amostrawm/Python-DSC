c9783ac00e9ccb8623f50e5d26e80a2ebff857ef
Amós Almeida <amsbeawp@gmail.com>
2023-02-21 18:38:35 -0300

    Adiciona funções internas

---
+++ Funcoes-internas.py
@@ -0,0 +1,14 @@
+# Funções para usar sem o uso de import
+
+abs()
+max()
+min()
+round()
+sum()
+
+# Modulos nativos que exigem import mas não exigem instalação
+mean()
+median()
+mode()
+stdev()
+variance()
