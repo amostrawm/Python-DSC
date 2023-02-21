84870a4383b85a19f1266db7e11c9f72800a9406
Amós Almeida <amsbeawp@gmail.com>
2023-02-21 18:16:17 -0300

    Ajustes

--- Listas.py
+++ Listas.py
@@ -1,5 +1,5 @@
 #Listas
-
+#Indexado em 0
 lst = [1, 2, 3, 4, 5]
 lst2 = [1, 2, 3, "4", True]
 lst3 = [12, [1, 2, 3, 4, 5], "a"]
@@ -11,3 +11,8 @@ print(lst[0])
 print(lst2[4])
 print(lst3[2])
 print(lst4[1])
+
+#Acessar o primeiro elemento
+print(lst[0])
+#Numero de elementos em uma lista  >>>>  len(lst)
+print(len(lst))
