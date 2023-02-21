3c4529c1de53d7f56fbdcf8e31c0aa5b6322644f
Amós Almeida <amsbeawp@gmail.com>
2023-02-21 11:51:39 -0300

    Adiciona estruturas de repetição

---
+++ main.py
@@ -0,0 +1,29 @@
+'''
+print("Teste")
+
+nota = 7
+frequencia = 90
+
+if nota >= 7 and frequencia > 70:
+    print("Aprovado")
+else:
+    print("Reprovado")
+
+
+if nota >= 7 or frequencia > 70:
+    print("Aprovado")
+else:
+    print("Reprovado")
+'''
+
+
+nota = 5
+
+if nota <= 4:
+    print("Reprovado")
+
+elif 4 < nota <= 6:
+    print("Exame")
+
+else:
+    print("Aprovado")
